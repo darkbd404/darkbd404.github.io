289f9c9711a2d925f3ee90ba393cc7ac06dcca4c
Dakrbd404 <198586681+darkbd404@users.noreply.github.com>
2026-07-10 23:20:19 +0600

    Create sw.js

---
+++ salam-ip-calling/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.message,
+        icon: 'https://cdn-icons-png.flaticon.com/512/5585/5585856.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/5585/5585856.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '1'
+        }
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
