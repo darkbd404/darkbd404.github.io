516aff3752b26d744c5e9bf5179e9672302ec21f
Dakrbd404 <198586681+darkbd404@users.noreply.github.com>
2026-07-11 00:12:32 +0600

    Update sw.js

--- salam-ip-calling/sw.js
+++ salam-ip-calling/sw.js
@@ -1,23 +1,16 @@
 self.addEventListener('push', function(event) {
-    const data = event.data.json();
+    const data = event.data ? event.data.json() : { title: 'Update', message: 'New notification' };
     const options = {
         body: data.message,
         icon: 'https://cdn-icons-png.flaticon.com/512/5585/5585856.png',
         badge: 'https://cdn-icons-png.flaticon.com/512/5585/5585856.png',
         vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: '1'
-        }
+        data: { dateOfArrival: Date.now(), primaryKey: '1' }
     };
-    event.waitUntil(
-        self.registration.showNotification(data.title, options)
-    );
+    event.waitUntil(self.registration.showNotification(data.title, options));
 });
 
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(
-        clients.openWindow('/')
-    );
+    event.waitUntil(clients.openWindow('/'));
 });
