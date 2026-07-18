a683c7c928bcde65be18eeb477eca5c7554cae93
Dakrbd404 <198586681+darkbd404@users.noreply.github.com>
2026-07-18 08:54:45 +0600

    Update sw.js

--- ip-calling-system/server/public/sw.js
+++ ip-calling-system/server/public/sw.js
@@ -1,9 +1,4 @@
-const CACHE_NAME = 'ip-call-v1';
+const CACHE_NAME = 'ip-call-v2';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.json'];
-
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-});
-self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
